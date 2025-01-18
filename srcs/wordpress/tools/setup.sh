2c3d74aa6d9d3ab519e316861aacd3939292fc1a
kfukuhar <kfukuhar@student.42tokyo.jp>
2025-01-18 13:10:12 +0900

    update .env

--- srcs/wordpress/tools/setup.sh
+++ srcs/wordpress/tools/setup.sh
@@ -10,8 +10,8 @@ if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
     
     # データベース設定の更新
     sed -i "s/database_name_here/$WORDPRESS_DB_NAME/" /var/www/html/wordpress/wp-config.php
-    sed -i "s/username_here/$WORDPRESS_DB_USER/" /var/www/html/wordpress/wp-config.php
-    sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/" /var/www/html/wordpress/wp-config.php
+    sed -i "s/username_here/$MYSQL_USER/" /var/www/html/wordpress/wp-config.php
+    sed -i "s/password_here/$MYSQL_PASSWORD/" /var/www/html/wordpress/wp-config.php
     sed -i "s/localhost/$WORDPRESS_DB_HOST/" /var/www/html/wordpress/wp-config.php
 
     # ユニークキーの設定
